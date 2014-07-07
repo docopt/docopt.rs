1562e4c10b619001b853684ecb3687ec9dc9df2b
Andrew Gallant <jamslam@gmail.com>
2014-07-06 21:14:18 -0400

    Rename example.

--- examples/hashmap.rs
+++ examples/hashmap.rs
@@ -2,7 +2,8 @@ extern crate docopt;
 
 use docopt::docopt;
 
-static docstr: &'static str = "
+fn main() {
+    let args = docopt("
 Naval Fate.
 
 Usage:
@@ -19,31 +20,7 @@ Options:
   --speed=<kn>  Speed in knots [default: 10].
   --moored      Moored (anchored) mine.
   --drifting    Drifting mine.
-";
-
-fn main() {
-    let args = docopt(docstr);
-    // If you invoke with:
-    //
-    //     no_macro ship Guardian move 100 150 --speed=15
-    //
-    // then you should get the following map:
-    //
-    //     --drifting  => Switch(false)
-    //     -h, --help  => Switch(false)
-    //     --moored    => Switch(false) 
-    //     --speed     => Plain(Some(15)) 
-    //     --version   => Switch(false) 
-    //     <name>      => List([Guardian]) 
-    //     <x>         => Plain(Some(100)) 
-    //     <y>         => Plain(Some(150)) 
-    //     mine        => Switch(false) 
-    //     move        => Switch(true) 
-    //     new         => Switch(false) 
-    //     remove      => Switch(false) 
-    //     set         => Switch(false) 
-    //     ship        => Switch(true) 
-    //     shoot       => Switch(false) 
+");
     println!("{}", args);
 
     // You can conveniently access values with `get_{bool,count,str,vec}`
