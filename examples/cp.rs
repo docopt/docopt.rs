05f2cd9080bfdf19397466aab2f71bb8e5d46154
Andrew Gallant <jamslam@gmail.com>
2014-11-02 11:55:19 -0500

    Add `cp` example with manual struct definition.

---
+++ examples/cp.rs
@@ -0,0 +1,28 @@
+extern crate serialize;
+extern crate docopt;
+
+use docopt::Docopt;
+
+// Write the Docopt usage string.
+static USAGE: &'static str = "
+Usage: cp [-a] <source> <dest>
+       cp [-a] <source>... <dir>
+
+Options:
+    -a, --archive  Copy everything.
+";
+
+#[deriving(Decodable, Show)]
+struct Args {
+    arg_source: Vec<String>,
+    arg_dest: String,
+    arg_dir: String,
+    flag_archive: bool,
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+    println!("{}", args);
+}
