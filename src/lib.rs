fb2361a30c79f0464360ed02992abe4a40721b61
Jason Ozias <jason.g.ozias@gmail.com>
2014-11-14 10:00:27 -0500

    Added 'use std::num::NumCast' to support removal from rust prelude in commit e965ba85ca689ad77f63b7f0af9d7e337dcb4825 on rust master

--- src/lib.rs
+++ src/lib.rs
@@ -221,6 +221,7 @@ use std::error::FromError;
 use std::fmt;
 use std::from_str::{FromStr, from_str};
 use std::num;
+use std::num::NumCast;
 use serialize::Decodable;
 use parse::Parser;
 use synonym::SynonymMap;
