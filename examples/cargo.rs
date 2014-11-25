d64e31bf7ec44660bb4f98f5ab79c42758ea6b90
Andrew Gallant <jamslam@gmail.com>
2014-11-24 20:12:45 -0500

    Add an example for sub-commands.

---
+++ examples/cargo.rs
@@ -0,0 +1,51 @@
+extern crate serialize;
+extern crate docopt;
+
+use docopt::Docopt;
+
+// Write the Docopt usage string.
+static USAGE: &'static str = "
+Rust's package manager
+
+Usage:
+    cargo <command> [<args>...]
+    cargo [options]
+
+Options:
+    -h, --help       Display this message
+    -V, --version    Print version info and exit
+    --list           List installed commands
+    -v, --verbose    Use verbose output
+
+Some common cargo commands are:
+    build       Compile the current project
+    clean       Remove the target directory
+    doc         Build this project's and its dependencies' documentation
+    new         Create a new cargo project
+    run         Build and execute src/main.rs
+    test        Run the tests
+    bench       Run the benchmarks
+    update      Update dependencies listed in Cargo.lock
+
+See 'cargo help <command>' for more information on a specific command.
+";
+
+#[deriving(Decodable, Show)]
+struct Args {
+    arg_command: Command,
+    arg_args: Vec<String>,
+    flag_list: bool,
+    flag_verbose: bool,
+}
+
+#[deriving(Decodable, Show)]
+enum Command {
+    Build, Clean, Doc, New, Run, Test, Bench, Update,
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+    println!("{}", args);
+}
